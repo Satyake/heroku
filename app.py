1f8a4d82e9bd8d88df0784c45f6b43a3cf6e031e
Satyake <35741572+Satyake@users.noreply.github.com>
2021-02-21 18:01:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask,render_template,request
 import numpy as np 
 import pickle
-
+app=Flask(__name__)
 model=pickle.load(open('LinearReg.pkl','rb'))
 
-app=Flask(__name__)
 @app.route ('/')
 def home():
     return render_template('index.html')
