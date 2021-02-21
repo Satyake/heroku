21f419df2b88c11b299cefa8aad244da8889b39d
Satyake <35741572+Satyake@users.noreply.github.com>
2021-02-21 18:05:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-from flask import Flask,render_template,request
-import numpy as np 
+import numpy as np
+from flask import Flask, request, jsonify, render_template
 import pickle
-app=Flask(__name__)
-model=pickle.load(open('LinearReg.pkl','rb'))
 
-@app.route ('/')
+app = Flask(__name__)
+model = pickle.load(open('LinearReg.pkl', 'rb'))
+
+@app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route('/predict',methods=['POST'])
 def predict():
-    features=[int(i) for i in request.form.values()]
-    features=[np.array(features)]
-    #features=features.reshape(-1,1)
-    predictions=model.predict(features)
-    outputs=predictions[0]
-    return render_template('index.html',prediction_text='Predicted is {}'.format(outputs))
-    
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
-if __name__== '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
